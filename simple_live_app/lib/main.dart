9f3b7c4da3a5254e56684af102cebf4eddb5346a
xuanxuan123xuan <xuanxuan123xuan@users.noreply.github.com>
2026-08-03 14:35:05 +0800

    fix: main.dart 补 import gestures（GestureBinding undefined）
    analyze 报 GestureBinding undefined，需 import 'package:flutter/gestures.dart'

--- simple_live_app/lib/main.dart
+++ simple_live_app/lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
