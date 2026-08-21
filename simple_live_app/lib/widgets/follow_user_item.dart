66b4d4bf51adbf8f1f7f8ec6cdcab6e4857c1b5e
xuanxuan123xuan <xuanxuan123xuan@users.noreply.github.com>
2026-08-21 19:17:29 +0800

    fix: restore follow cover cropping

--- simple_live_app/lib/widgets/follow_user_item.dart
+++ simple_live_app/lib/widgets/follow_user_item.dart
@@ -670,8 +670,6 @@ class FollowUserItem extends StatelessWidget {
                   coverImage,
                   fit: BoxFit.cover,
                   borderRadius: radius,
-                  cacheWidth: 640,
-                  cacheHeight: 360,
                   clearMemoryCacheWhenDispose: true,
                   imageCacheName: NetImage.liveCoverCacheName,
                   cacheMaxAge: const Duration(minutes: 10),
